3a76a16fdebfa86b9cc7bd138b203a8f02fcdc85
Ben51Degrees <ben@51degrees.com>
2023-03-28 14:01:52 +0100

    CLEANUP: Uncommented submodule update steps

--- nightly-submodule-update.ps1
+++ nightly-submodule-update.ps1
@@ -18,9 +18,9 @@ if ($LASTEXITCODE -eq 0) {
     
     ./steps/commit-changes.ps1 -RepoName $RepoName -Message "REF: Updated submodules."
 
-    # ./steps/push-changes.ps1 -RepoName $RepoName -Branch $BranchName
+    ./steps/push-changes.ps1 -RepoName $RepoName -Branch $BranchName
 
-    # ./steps/pull-request-to-main.ps1 -RepoName $RepoName -Message "Updated submodule."
+    ./steps/pull-request-to-main.ps1 -RepoName $RepoName -Message "Updated submodule."
 }
 else {
     Write-Host "No submodule changes, so not creating a pull request."
