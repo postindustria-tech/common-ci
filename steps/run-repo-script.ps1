c6e98fe0ed169cd0e8ffedcc89f8689010babd37
Ben51Degrees <ben@51degrees.com>
2023-04-03 18:36:13 +0100

    BUILD: Pass named script parameters in as a hash table rather than an array.

--- steps/run-repo-script.ps1
+++ steps/run-repo-script.ps1
@@ -12,16 +12,16 @@ $Script = [IO.Path]::Combine($RepoPath, "ci", $ScriptName)
 
 $ScriptParameters = (Get-Command -Name $Script).Parameters
 
-$Parameters = @()
+$Parameters = @{}
 
 foreach ($Option in $Options.GetEnumerator()) {
     if ($ScriptParameters.ContainsKey($Option.Key)) {
-        $Parameters += "-$($Option.Key)",  $Option.Value
+        $Parameters.Add($Option.Key, $Option.Value)
     }
 }
 
 Write-Output "Running script '$Script' with parameters: $Parameters"
 
-. $Script -Options $Parameters
+. $Script @Parameters
 
 exit $LASTEXITCODE
