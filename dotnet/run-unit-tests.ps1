b573be39aa70a209137fff1d9787973d6cf9c8f5
Ben51Degrees <ben@51degrees.com>
2023-04-04 10:43:03 +0100

    TEST: Removed no-restore

--- dotnet/run-unit-tests.ps1
+++ dotnet/run-unit-tests.ps1
@@ -17,7 +17,7 @@ try {
 
     Write-Output "Testing $($Options.Name)"
     
-    dotnet test $ProjectDir --no-build --no-restore --results-directory test-results --blame-crash -l "trx;LogFilePrefix=$Name" -c $Configuration -a $Arch
+    dotnet test $ProjectDir --no-build --results-directory test-results --blame-crash -l "trx;LogFilePrefix=$Name" -c $Configuration -a $Arch
 
 }
 finally {
