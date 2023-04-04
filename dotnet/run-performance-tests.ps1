df7904bdd63ab33d6491a93c609d351628652628
Ben51Degrees <ben@51degrees.com>
2023-04-04 13:57:19 +0100

    TEST: Added scripts for running performance and integration tests.

---
+++ dotnet/run-performance-tests.ps1
@@ -0,0 +1,30 @@
+
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [string]$ProjectDir = ".",
+    [string]$Name = "Release_x64",
+    [string]$Configuration = "Release",
+    [string]$Arch = "x64"
+)
+
+$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
+
+Write-Output "Entering '$RepoPath'"
+Push-Location $RepoPath
+
+try {
+
+    Write-Output "Testing $($Options.Name)"
+    
+    dotnet test $ProjectDir --results-directory "test-results/performance/$Name"--filter "FullyQualifiedName~Performance|TestCategory=Performance" --blame-crash -l "trx" -c $Configuration -a $Arch
+
+}
+finally {
+
+    Write-Output "Leaving '$RepoPath'"
+    Pop-Location
+
+}
+
+exit $LASTEXITCODE
