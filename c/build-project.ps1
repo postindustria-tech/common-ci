e0af017bc477a663774ba0068649a1333ddf0b8c
Ben51Degrees <ben@51degrees.com>
2023-04-05 16:00:41 +0100

    FEAT: Added msbuild option to c build script.

--- c/build-project.ps1
+++ c/build-project.ps1
@@ -4,28 +4,55 @@ param(
     [string]$ProjectDir = ".",
     [string]$RepoName,
     [string]$Name = "Release_x64",
-    [string]$Configuration = "Release"
+    [string]$Configuration = "Release",
+    [string]$BuildMethod = "cmake"
 )
 
-$RepoPath = [IO.Path]::Combine($pwd, $RepoName, $ProjectDir, "build")
+if ($BuildMethod -eq "cmake") {
 
-mkdir $RepoPath
+    $RepoPath = [IO.Path]::Combine($pwd, $RepoName, $ProjectDir, "build")
+    mkdir $RepoPath
 
-Write-Output "Entering '$RepoPath'"
-Push-Location $RepoPath
-
-try {
-
-    Write-Output "Building '$Name'"
+    Write-Output "Entering '$RepoPath'"
+    Push-Location $RepoPath
     
-    cmake .. -DCMAKE_BUILD_TYPE=$Configuration 
-    cmake --build .
-
+    try {
+    
+        Write-Output "Building '$Name'"
+        
+        cmake .. -DCMAKE_BUILD_TYPE=$Configuration 
+        cmake --build .
+    
+    }
+    finally {
+    
+        Write-Output "Leaving '$RepoPath'"
+        Pop-Location
+    
+    }
 }
-finally {
+elseif ($BuildMethod -eq "msbuild") {
 
-    Write-Output "Leaving '$RepoPath'"
-    Pop-Location
+    $RepoPath = [IO.Path]::Combine($pwd, $RepoName, $ProjectDir)
+    
+    Write-Output "Entering '$RepoPath'"
+    Push-Location $RepoPath
+    
+    try {
+
+        msbuild $ProjectDir /p:Configuration=$Configuration /property:Platform=$Arch
+
+    }
+    finally {
+
+        Write-Output "Leaving '$RepoPath'"
+        Pop-Location
+
+    }
+}
+else {
+
+    Write-Error "The build method '$BuildMethod' is not supported."
 
 }
 
