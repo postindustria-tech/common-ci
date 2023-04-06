15f2ab7d8762183c1de16c6c6c048841ee913615
Ben51Degrees <ben@51degrees.com>
2023-04-06 11:54:42 +0100

    FEAT: Log the path where msbuild was found.

--- environments/setup-msbuild.ps1
+++ environments/setup-msbuild.ps1
@@ -2,4 +2,5 @@
 Invoke-WebRequest -Uri https://github.com/microsoft/vswhere/releases/download/3.1.1/vswhere.exe -OutFile vswhere.exe
 $MsBuild = .\vswhere.exe -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
 $MsBuilsPath = $(Get-ChildItem $MsBuild).Directory.FullName
+Write-Output "Found msbuild in '$MsBuildPath'"
 echo $MsBuildPath | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
