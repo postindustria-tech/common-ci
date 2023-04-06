f575c65ca9816ab16e847d227829732c85cd8058
Ben51Degrees <ben@51degrees.com>
2023-04-06 12:31:34 +0100

    BUG: Corrected variable naming.

--- environments/setup-msbuild.ps1
+++ environments/setup-msbuild.ps1
@@ -1,7 +1,6 @@
 
 Invoke-WebRequest -Uri https://github.com/microsoft/vswhere/releases/download/3.1.1/vswhere.exe -OutFile vswhere.exe
-.\vswhere.exe -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
 $MsBuild = .\vswhere.exe -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
-$MsBuilsPath = $(Get-ChildItem $MsBuild).Directory.FullName
+$MsBuildPath = $(Get-ChildItem $MsBuild).Directory.FullName
 Write-Output "Found msbuild in '$MsBuildPath'"
 echo $MsBuildPath | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
