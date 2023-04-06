d266954b8229e6d498245dd714a23d159f4e755f
ben51degrees <ben@51degrees.com>
2023-04-06 18:08:41 +0100

    Delete setup-hash-license.ps1

--- environments/setup-hash-license.ps1
+++
@@ -1,3 +0,0 @@
-
-$Secrets = ConvertFrom-Json $env:SECRETS_CONTEXT -AsHashtable 
-echo DEVICE_DETECTION_KEY=$($Secrets.DEVICE_DETECTION_KEY) | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
