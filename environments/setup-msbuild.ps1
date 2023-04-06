cca9eca04c2b41360833bd77cb0985c4710fd2f0
Ben51Degrees <ben@51degrees.com>
2023-04-06 11:32:18 +0100

    BUG: Use the msbuild dir, not the file.

--- environments/setup-msbuild.ps1
+++ environments/setup-msbuild.ps1
@@ -1,4 +1,5 @@
 
 Invoke-WebRequest -Uri https://github.com/microsoft/vswhere/releases/download/3.1.1/vswhere.exe -OutFile vswhere.exe
 $MsBuild = .\vswhere.exe -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
-echo $MsBuild | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+$MsBuilsPath = $(Get-ChildItem $MsBuild).Directory.FullName
+echo $MsBuildPath | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
