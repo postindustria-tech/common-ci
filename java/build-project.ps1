40845d037c9e4f79171b3a7ec25d1be8346f659f
Artur <artur@51degrees.com>
2023-04-11 12:55:25 +0100

    WIP: testing differen java sdk

--- java/build-project.ps1
+++ java/build-project.ps1
@@ -12,6 +12,17 @@ Write-Output "Entering '$RepoPath'"
 Push-Location $RepoPath
 
 try {
+    Write-Host $env:JAVA_HOME_8_X64
+    
+    # Set the JAVA_HOME environment variable
+    $env:JAVA_HOME = $env:JAVA_HOME_8_X64
+
+    # Add the Java binary directory to the system PATH
+    $env:Path += ";" + "$env:JAVA_HOME\bin"
+
+    # Verify that the correct version of Java is being used
+    java -version
+
     #-Dhttps.protocols=TLSv1.2
     Write-Output "Building '$Name'"
     mvn compile -f pom.xml -DXmx2048m --no-transfer-progress
