58192abb75654007ac389d238b69241c95cc78d3
Ben51Degrees <ben@51degrees.com>
2023-04-14 16:34:25 +0100

    FEAT: Take token as a parameter

--- steps/configure-git.ps1
+++ steps/configure-git.ps1
@@ -1,5 +1,7 @@
-
-git config --global url.https://$($env:GITHUB_TOKEN)@github.com/.insteadOf https://github.com/
+param (
+    $GitHubToken
+)
+git config --global url.https://$GitHubToken@github.com/.insteadOf https://github.com/
 git config --global user.email "CI@51Degrees.com"
 git config --global user.name "51DCI"
 git config --global filter.lfs.smudge "git-lfs smudge --skip -- %f"
