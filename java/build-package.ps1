e3caa5ee2ef004a92034411c0ed007be59a53689
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-25 16:28:02 +0100

    Update build-package.ps1

--- java/build-package.ps1
+++ java/build-package.ps1
@@ -10,11 +10,15 @@ param(
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
 # Path for the locally installed packages from which they will be uploaded to artifacts
-$PackagePath = "$RepoPath/package/"
+$PackagePath = "$RepoPath/package"
 
 $MavenLocalRepoPath = mvn help:evaluate -Dexpression="settings.localRepository" -q -DforceStdout
 
+Write-Output $MavenLocalRepoPath
+
 $MavenLocal51DPath = [IO.Path]::Combine($MavenLocalRepoPath, "com", "51degrees")
+
+Write-Output $MavenLocal51DPath
 
 Write-Output "Entering '$RepoPath'"
 Push-Location $RepoPath
@@ -29,7 +33,7 @@ try {
     Write-Output "Building '$Name'"
     mvn install -f pom.xml -DXmx2048m -DskipTests --no-transfer-progress '-Dhttps.protocols=TLSv1.2' -DfailIfNoTests=false
 
-    New-Item -path $PackagePath -ItemType Directory -Force 
+    New-Item -Path $PackagePath -ItemType Directory -Force 
     $LocalRepoPackages = Get-ChildItem -Path $MavenLocal51DPath
     Copy-Item -Path $LocalRepoPackages -Destination "$PackagePath"
 
