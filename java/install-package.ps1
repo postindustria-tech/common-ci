68feb073a6174e2ced569a5b3edacaf171319adf
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-26 11:24:36 +0100

    Update install-package.ps1

--- java/install-package.ps1
+++ java/install-package.ps1
@@ -1,4 +1,3 @@
-
 param(
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
@@ -6,30 +5,38 @@ param(
     [string]$Name
 )
 
+# Combine the current working directory with the repository name
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
-# Path for the locally installed packages from which they will be uploaded to artifacts
-$PackagePath = "$RepoPath/package/"
+# Define the path for locally installed packages to be uploaded to artifacts
+$PackagePath = Join-Path -Path $RepoPath -ChildPath "package"
 
-$LocalRepoPath = [IO.Path]::Combine($home, ".m2", "repository", "com", "51degrees")
+# Get the Maven local repository path
+$MavenLocalRepoPath = mvn help:evaluate -Dexpression="settings.localRepository" -q -DforceStdout
 
+# Define the path for the local 51degrees Maven repository
+$MavenLocal51DPath = Join-Path -Path $MavenLocalRepoPath -ChildPath "com\51degrees"
+
+# Display the repository path and enter it
 Write-Output "Entering '$RepoPath'"
 Push-Location $RepoPath
 
 try {
+    # Copy the content of the package path to the Maven local repository
+    Copy-Item -Path $PackagePath -Destination (Join-Path -Path $MavenLocalRepoPath -ChildPath "com") -Recurse
 
-    New-Item -path $LocalRepoPath -ItemType Directory -Force 
-    # Copy files over from target to package-files folder
-    $RepoPackages = Get-ChildItem -Path $PackagePath
-    Copy-Item -Path $RepoPackages -Destination "$LocalRepoPath"
+    # Rename the folder in the Maven local repository
+    Rename-Item -Path (Join-Path -Path $MavenLocalRepoPath -ChildPath "com\package") -NewName "51degrees"
 
-
+    # Display the content of the local 51degrees Maven repository
+    Write-Output "Content of the local 51d Maven Repository: "
+    Get-ChildItem $MavenLocal51DPath
 }
 finally {
-
+    # Leave the repository path and display it
     Write-Output "Leaving '$RepoPath'"
     Pop-Location
-
 }
 
+# Exit the script with the last exit code
 exit $LASTEXITCODE
