fad8c40de529306fdf6244c963f83593e437164c
Artur <artur@51degrees.com>
2023-04-27 12:05:37 +0100

    REFACT: Move signing packages to build stage

--- java/build-package.ps1
+++ java/build-package.ps1
@@ -4,8 +4,19 @@ param(
     [string]$RepoName,
     [string]$ProjectDir = ".",
     [string]$Name,
+    [Parameter(Mandatory=$true)]
     [string]$Version,
-    [string]$ExtraArgs
+    [string]$ExtraArgs,
+    [Parameter(Mandatory=$true)]
+    [string]$JavaGpgKeyPassphrase,
+    [Parameter(Mandatory=$true)]
+    [string]$CodeSigningCert,
+    [Parameter(Mandatory=$true)]
+    [string]$JavaPGP,
+    [Parameter(Mandatory=$true)]
+    [string]$CodeSigningCertAlias,
+    [Parameter(Mandatory=$true)]
+    [string]$CodeSigningCertPassword
 )
 
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
@@ -26,8 +37,26 @@ try {
     Write-Output "Setting package version to '$Version'"
     mvn versions:set -DnewVersion="$Version"
 
+    # Set file names
+    $settingsFile = "stagingsettings.xml"
+    $CodeSigningCertFile = "51Degrees Private Code Signing Certificate.pfx"
+    $JavaPGPFile = "Java Maven GPG Key Private.pgp"
+
+    Write-Output "Writing Settings File"
+    $SettingsContent = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($MavenSettings))
+    Set-Content -Path $settingsFile -Value $SettingsContent
+
+    Write-Output "Writing PFX File"
+    $CodeCertContent = [System.Convert]::FromBase64String($CodeSigningCert)
+    Set-Content $CodeSigningCertFile -Value $CodeCertContent -AsByteStream
+
+    Write-Output "Writing PGP File"
+    Set-Content -Path $JavaPGPFile -Value $JavaPGP
+
+    echo $JavaGpgKeyPassphrase | gpg --import --batch --yes --passphrase-fd 0 $JavaPGPFile
+
     Write-Output "Building '$Name'"
-    mvn install -f pom.xml -DXmx2048m -DskipTests --no-transfer-progress '-Dhttps.protocols=TLSv1.2' -DfailIfNoTests=false $ExtraArgs
+    mvn install -f pom.xml -DXmx2048m -DskipTests --no-transfer-progress '-Dhttps.protocols=TLSv1.2' -DfailIfNoTests=false $ExtraArgs -Dskippackagesign=false "-Dgpg.passphrase=$JavaGpgKeyPassphrase" -Dkeystore=$CodeSigningCertFile -Dalias=$CodeSigningCertAlias -Dkeypass=$CodeSigningCertPassword -Dkeystorepass=$CodeSigningCertPassword
 
     $LocalRepoPackages = Get-ChildItem -Path $MavenLocal51DPath
     Write-Output "Maven Local 51d Repo:"
--- java/publish-package-maven.ps1
+++ java/publish-package-maven.ps1
@@ -1,17 +1,9 @@
 param (
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
+    [Parameter(Mandatory=$true)]
     [string]$MavenSettings,
     [Parameter(Mandatory=$true)]
-    [string]$JavaGpgKeyPassphrase,
-    [Parameter(Mandatory=$true)]
-    [string]$CodeSigningCert,
-    [Parameter(Mandatory=$true)]
-    [string]$JavaPGP,
-    [Parameter(Mandatory=$true)]
-    [string]$CodeSigningCertAlias,
-    [Parameter(Mandatory=$true)]
-    [string]$CodeSigningCertPassword,
     $Version
 )
 
@@ -25,28 +17,10 @@ try {
     Write-Output "Setting version to '$Version'"
     mvn versions:set -DnewVersion="$Version"
 
-    # Set file names
-    $settingsFile = "stagingsettings.xml"
-    $CodeSigningCertFile = "51Degrees Private Code Signing Certificate.pfx"
-    $JavaPGPFile = "Java Maven GPG Key Private.pgp"
-
-    Write-Output "Writing Settings File"
-    $SettingsContent = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($MavenSettings))
-    Set-Content -Path $settingsFile -Value $SettingsContent
-
-    Write-Output "Writing PFX File"
-    $CodeCertContent = [System.Convert]::FromBase64String($CodeSigningCert)
-    Set-Content $CodeSigningCertFile -Value $CodeCertContent -AsByteStream
-
-    Write-Output "Writing PGP File"
-    Set-Content -Path $JavaPGPFile -Value $JavaPGP
-
-
-    echo $JavaGpgKeyPassphrase | gpg --import --batch --yes --passphrase-fd 0 $JavaPGPFile
 
     Write-Output "Deploying to Nexus staging"
     
-    mvn deploy -DdeployOnly -DskipTests -s $settingsFile --no-transfer-progress "-Dhttps.protocols=TLSv1.2" -Dskippackagesign=false "-Dgpg.passphrase=$JavaGpgKeyPassphrase" -Dkeystore=$CodeSigningCertFile -Dalias=$CodeSigningCertAlias -Dkeypass=$CodeSigningCertPassword -Dkeystorepass=$CodeSigningCertPassword
+    mvn deploy -DdeployOnly -DskipTests -s $settingsFile --no-transfer-progress "-Dhttps.protocols=TLSv1.2" 
 
     if ($($Version.EndsWith("SNAPSHOT")) -eq $False) {
 
