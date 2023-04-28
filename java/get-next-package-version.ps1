2e306b6b226b9b8c27826df9e3780ac51b8b42c8
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-28 09:42:51 +0100

    Update get-next-package-version.ps1

--- java/get-next-package-version.ps1
+++ java/get-next-package-version.ps1
@@ -9,7 +9,7 @@ $GitVersionConfigPath = [IO.Path]::Combine($pwd, "java", "gitversion.yml")
 
 ./steps/get-next-package-version.ps1 -RepoName $RepoName -VariableName $VariableName -GitVersionConfigPath $GitVersionConfigPath
 
-$assemblySemVer = (Get-Variable -Name $VariableName -Scope Global).Value
+$assemblySemVer = (Get-Variable -Name $VariableName).Value.AssemblySemVer
 Set-Variable -Name $VariableName -Value $assemblySemVer -Scope Global
 
 exit $LASTEXITCODE
