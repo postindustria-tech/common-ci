4cef95b14aa911e54e4ed5060cc62ecba719fbf1
Ben51Degrees <ben@51degrees.com>
2023-05-05 17:25:29 +0100

    FEAT: Added scripts for generating documentation.

---
+++ nightly-documentation-update.ps1
@@ -0,0 +1,53 @@
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [string]$GitHubToken
+)
+
+. ./constants.ps1
+
+Write-Output "::group::Configure Git"
+./steps/configure-git.ps1 -GitHubToken $GitHubToken
+Write-Output "::endgroup::"
+
+Write-Output "::group::Clone $RepoName"
+./steps/clone-repo.ps1 -RepoName $RepoName
+Write-Output "::endgroup::"
+
+Write-Output "::group::Clone Tools"
+./steps/clone-repo.ps1 -RepoName "tools"
+Write-Output "::endgroup::"
+
+Write-Output "::group::Clone Documentation"
+if ($RepoName -ne "documentation") {
+    ./steps/clone-repo.ps1 -RepoName "documentation"
+}
+Write-Output "::endgroup::"
+
+Write-Output "::group::Generate Documentation"
+./steps/generate-documentation.ps1 -RepoName $RepoName
+Write-Output "::endgroup::"
+
+Write-Output "::group::Has Changed"
+./steps/has-changed.ps1 -RepoName $RepoName
+Write-Output "::endgroup::"
+
+if ($LASTEXITCODE -eq 0) {
+    
+    Write-Output "::group::Commit Changes"
+    ./steps/commit-changes.ps1 -RepoName $RepoName -Message "REF: Updated documentation."
+    Write-Output "::endgroup::"
+
+    Write-Output "::group::Push Changes"
+    ./steps/push-changes.ps1 -RepoName $RepoName -Branch gh-pages
+    Write-Output "::endgroup::"
+
+}
+else {
+
+    Write-Host "No property changes, so not pushing changes."
+
+}
+
+exit 0
