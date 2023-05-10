aa0661cab88b8e8619a85b5f5b8a97bd2703a18b
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-10 12:10:31 +0100

    Update build-project.ps1

--- java/build-project.ps1
+++ java/build-project.ps1
@@ -3,8 +3,7 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
     [string]$ProjectDir = ".",
-    [string]$Name,
-    [string]$Version
+    [string]$Name
 )
 
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
@@ -12,14 +11,8 @@ $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 Write-Output "Entering '$RepoPath'"
 Push-Location $RepoPath
 
-try {
-    if(![string]::IsNullOrWhitespace($Version)){
-        Write-Output "Setting package version to '$Version'"
-        mvn versions:set -DnewVersion="$Version"
-    }
-    
-    Write-Output "Building '$Name'"
-    mvn install -f pom.xml -DXmx2048m -DskipTests --no-transfer-progress '-Dhttps.protocols=TLSv1.2' -DfailIfNoTests=false
+Write-Output "Building '$Name'"
+mvn install -f pom.xml -DXmx2048m -DskipTests --no-transfer-progress '-Dhttps.protocols=TLSv1.2' -DfailIfNoTests=false
 
 }
 finally {
