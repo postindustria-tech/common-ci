139fe44ea882b18575eb35586a70c413a1182564
Artur <artur@51degrees.com>
2023-05-11 11:47:32 +0100

    FEAT: Enable long paths

--- steps/configure-git.ps1
+++ steps/configure-git.ps1
@@ -6,3 +6,4 @@ git config --global user.email "CI@51Degrees.com"
 git config --global user.name "51DCI"
 git config --global filter.lfs.smudge "git-lfs smudge --skip -- %f"
 git config --global filter.lfs.process "git-lfs filter-process --skip"
+git config --global core.longpaths true
