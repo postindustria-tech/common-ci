3de3acea25ae56fbdff40948ac8bfe2884a1b419
51DCI <CI@51Degrees.com>
2023-05-16 12:03:02 +0100

    FEAT: Prevent automation for PRs which have a reviewer assigned, but the reviewer has not approved yet.

--- steps/get-pull-requests.ps1
+++ steps/get-pull-requests.ps1
@@ -11,36 +11,72 @@ $env:GITHUB_TOKEN="$GitHubToken"
 
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
+$OrgUsers = hub api /orgs/51degrees/members | ConvertFrom-JSON
+
+function IsOrgUser {
+    param (
+        [string]$UserId
+    )
+    foreach ($OrgUser in $OrgUsers) {
+        if ($OrgUser.id -eq $UserId) {
+            return $True
+        }
+    }
+    return $False
+}
+
+function HasReviewed {
+    param (
+        [string]$UserId,
+        $Reviews
+    )
+    foreach ($Review in $Reviews) {
+        if ($Review.user.id -eq $UserId -and
+            $Review.state -eq 'APPROVED') {
+                return $True
+        }
+    }
+    return $False
+}
+
 function ShouldRun {
     param (
         [string]$RepoName,
         [string]$Id
     )
+    $Allowed = $True
+    $Reviews = hub api /repos/51degrees/$RepoName/pulls/$Id/reviews | ConvertFrom-JSON
     $Pr = hub api /repos/51degrees/$RepoName/pulls/$Id | ConvertFrom-Json
     if ($Pr.author_association -eq 'OWNER' -or
         $Pr.author_association -eq 'COLLABORATOR' -or
         $Pr.author_association -eq 'CONTRIBUTOR' -or
         $Pr.author_association -eq 'MEMBER') {
         # The author is one of the above, so return true
-        return $True
+        Write-Information "The creator is '$($Pr.author_association)', so allow automation"
+        $Allowed = $True
     }
     else {
         # The author is not one of the above, so check that
         # the PR has been approved
-        $Reviews = hub api /repos/51degrees/$RepoName/pulls/$Id/reviews | ConvertFrom-JSON
-        if ($Reviews.state -eq 'APPROVED') {
-            $OrgUsers = hub api /orgs/51degrees/members | ConvertFrom-JSON
-            foreach ($OrgUser in $OrgUsers) {
-                if ($OrgUser.id -eq $Reviews.user.id) {
-                    # The PR has been approved by a 51Degrees user,
-                    # so return true
-                    return $True
+        foreach ($Review in $Reviews) {
+            if ($Review.state -eq 'APPROVED') {
+                if (IsOrgUser -UserId $Review.user.id) {
+                    Write-Information "The creator is external, but has been approved by '$($Review.user.id)', so allow automation"
+                    $Allowed = $True
                 }
             }
         }
     }
-    # The user is external, and the PR has not been approved yet
-    return $False
+    if ($Pr.requested_reviewers.Count -gt 0) {
+        foreach ($Reviewer in $Pr.requested_reviewers) {
+            $User = hub api /users/$($Reviewer.login) | ConvertFrom-Json
+            if ($(HasReviewed -UserId $User.id -Reviews $Reviews) -eq $False) {
+                Write-Information "The user '$($User.login)' has not approved, so do not run automation"
+                $Allowed = $False
+            }
+        }
+    }
+    return $Allowed
 }
 
 Write-Output "Entering '$RepoPath'"
@@ -59,6 +95,7 @@ try {
 
         foreach ($Id in $Ids) {
             # Only select PRs which are eligeble for automation.
+            Write-Output "Checking PR #$Id"
             if (ShouldRun -RepoName $RepoName -Id $Id)
             {
                 $ValidIds += $Id
