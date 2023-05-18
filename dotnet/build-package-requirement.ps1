b8d134eb3250c3baa4265a6bbb1822782ec5574d
Artur <artur@51degrees.com>
2023-05-18 11:12:39 +0100

    FEAT: add architecture as a parameter for building native code

--- dotnet/build-package-requirement.ps1
+++ dotnet/build-package-requirement.ps1
@@ -1,9 +1,10 @@
 param(
     [string]$ProjectDir = ".",
     [string]$Configuration = "CoreRelease",
-    [string]$RepoName
+    [string]$RepoName,
+    [string]$Arch
 )
 
-./cxx/build-project.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Configuration $Configuration
+./cxx/build-project.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Configuration $Configuration -Arch $Arch
 
 exit $LASTEXITCODE
