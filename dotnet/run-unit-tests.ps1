1dd7e5f8e7699f9eb3e86b938b0c52d3fa84a535
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-24 14:05:18 +0100

    Update run-unit-tests.ps1

--- dotnet/run-unit-tests.ps1
+++ dotnet/run-unit-tests.ps1
@@ -20,8 +20,12 @@ try {
     $skipPattern = "*performance*"
     Write-Output "Testing '$Name'"
     if ($BuildMethod -eq "dotnet"){
-        dotnet test $ProjectDir --results-directory "test-results/unit/$Name" --blame-crash -l "trx" -c $Configuration --no-build /p:Platform=$Arch
-
+        Get-ChildItem -Path $RepoPath -Recurse -File | ForEach-Object {
+            if (($_.DirectoryName -like "*\bin\*" -and $_.Name -notlike $skipPattern) -and ($_.Name -match "$Filter")) {
+                Write-Output "Testing Assembly: '$_'"
+                dotnet test $_.FullName --results-directory "test-results/unit/$Name" --blame-crash -l "trx" -c $Configuration --no-build /p:Platform=$Arch
+            }
+        }
     }
     else{
 
