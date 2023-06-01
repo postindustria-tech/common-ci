131799da3884eb4ffcce9fa3593577d0d1343e59
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-06-01 12:45:06 +0100

    Update publish-package-nuget.ps1

--- dotnet/publish-package-nuget.ps1
+++ dotnet/publish-package-nuget.ps1
@@ -4,34 +4,26 @@ param(
     [string]$RepoName,
     [string]$ProjectDir = ".",
     [string]$Name = "Release_x64",
-    #[Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$true)]
     [string]$ApiKey
 )
 
-$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
+$PackagePath = [IO.Path]::Combine($pwd, "package")
 
-Write-Output "Entering '$RepoPath'"
-Push-Location $RepoPath
+Write-Output "Entering '$PackagePath'"
+Push-Location $PackagePath
 
 try {
 
     Write-Output "Releasing package for '$Name'"
-
-    $packageDirectory = [IO.Path]::Combine((Get-Item $RepoPath).Parent.FullName, "package")
-    $packagePaths = Get-ChildItem -Path $packageDirectory -Filter "*.nupkg" -File | ForEach-Object { $_.FullName }
-    $packageString = $packagePaths -join ' '
-
-    # This is set to internal feed for now for testing.
-    dotnet nuget push --source "https://51degrees.pkgs.visualstudio.com/_packaging/pipeline-insider/nuget/v3/index.json" --api-key az $packageString
-
-    # This releases the package to nuget publicly uncomment when ready. 
-    #dotnet nuget push $packageString --source https://api.nuget.org/v3/index.json --api-key $ApiKey
-
+    
+    
+    dotnet nuget push "*.nupkg" --source https://api.nuget.org/v3/index.json --api-key $ApiKey
 
 }
 finally {
 
-    Write-Output "Leaving '$RepoPath'"
+    Write-Output "Leaving '$PackagePath'"
     Pop-Location
 
 }
