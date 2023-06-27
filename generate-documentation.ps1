3b4f2b5d6cfc71de274b8763eb85dee581cd4fde
Ben51Degrees <Ben@51Degrees.com>
2023-06-27 11:05:50 +0100

    FEAT: Added workflow to generate and upload a preview of generated docs.

---
+++ generate-documentation.ps1
@@ -0,0 +1,73 @@
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [Parameter(Mandatory=$true)]
+    [string]$OrgName,
+    [string]$GitHubUser = "",
+    [string]$GitHubEmail = "",
+    [string]$GitHubToken,
+    [bool]$SeparateExamples= $False
+)
+
+. ./constants.ps1
+
+if ($GitHubUser -eq "") {
+    $GitHubUser = $DefaultGitUser
+}
+if ($GitHubEmail -eq "") {
+    $GitHubEmail = $DefaultGitEmail
+}
+
+# This token is used by the hub command.
+Write-Output "Setting GITHUB_TOKEN"
+$env:GITHUB_TOKEN="$GitHubToken"
+
+Write-Output "::group::Configure Git"
+./steps/configure-git.ps1 -GitHubToken $GitHubToken -GitHubUser $GitHubUser -GitHubEmail $GitHubEmail
+Write-Output "::endgroup::"
+
+Write-Output "::group::Clone $RepoName"
+./steps/clone-repo.ps1 -RepoName $RepoName -OrgName $OrgName
+Write-Output "::endgroup::"
+
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
+
+if ($SeparateExamples){
+    $ExamplesRepo = "$RepoName-examples"
+    
+    Write-Output "::group::Clone $ExamplesRepo"
+    ./steps/clone-repo.ps1 -RepoName $ExamplesRepo -OrgName $OrgName -DestinationDir $RepoName
+    Write-Output "::endgroup::"
+    
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }    
+}
+
+Write-Output "::group::Clone Tools"
+./steps/clone-repo.ps1 -RepoName "tools" -OrgName $OrgName
+Write-Output "::endgroup::"
+
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
+Write-Output "::group::Clone Documentation"
+if ($RepoName -ne "documentation") {
+    ./steps/clone-repo.ps1 -RepoName "documentation" -OrgName $OrgName
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }    
+}
+Write-Output "::endgroup::"
+
+Write-Output "::group::Generate Documentation"
+./steps/generate-documentation.ps1 -RepoName $RepoName
+Write-Output "::endgroup::"
+
+exit $LASTEXITCODE
