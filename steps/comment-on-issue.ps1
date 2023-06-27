5425a668a26bde0d869abb73bf6f2340dd41b56c
ben51degrees <ben@51degrees.com>
2023-06-27 12:46:45 +0100

    Update comment-on-issue.ps1

--- steps/comment-on-issue.ps1
+++ steps/comment-on-issue.ps1
@@ -6,4 +6,8 @@ param(
     [string]$GitHubToken
 )
 
+# This token is used by the hub command.
+Write-Output "Setting GITHUB_TOKEN"
+$env:GITHUB_TOKEN="$GitHubToken"
+
 hub api /repos/$OrgName/$RepoName/issues/$Id/comments -X POST -f `"body=$Message`"
