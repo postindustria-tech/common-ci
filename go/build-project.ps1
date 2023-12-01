021cfff8226fbaad433d9aefc704181e0c839241
Alexander Popel <alexander.popel@postindustria.com>
2023-12-01 10:54:52 +0200

    go: remove prebuild step

--- go/build-project.ps1
+++ go/build-project.ps1
@@ -1,17 +1,1 @@
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName
-)
-
-Push-Location $RepoName
-try {
-    Write-Output "Building the submodule..."
-    $env:MAKEFLAGS = "-j$([Environment]::ProcessorCount)"
-    if ($IsMacOs) {
-        $env:MACOSX_DEPLOYMENT_TARGET = sw_vers -productVersion
-        Write-Output "macOS deployment target is $($env:MACOSX_DEPLOYMENT_TARGET)"
-    }
-    dd/scripts/prebuild.ps1 || $(throw "prebuild failed")
-} finally {
-    Pop-Location
-}
+Write-Output "Tests will build the project on demand, prebuild not required."
