3c3a095a8fb89ecfafafff7cb4b2340514a2c565
Alexander Popel <alexander.popel@postindustria.com>
2023-12-14 14:20:33 +0200

    python: temporarlily use TestPyPI

--- python/publish-package-pypi.ps1
+++ python/publish-package-pypi.ps1
@@ -9,7 +9,7 @@ $env:TWINE_USERNAME = "__token__"
 pip install twine || $(throw "pip install failed")
 if (!$DryRun) {
     $env:TWINE_PASSWORD = $Keys.PypiToken
-    twine upload (Get-ChildItem -Path package) || $(throw "package upload failed")
+    twine upload --repository testpypi (Get-ChildItem -Path package) || $(throw "package upload failed")
 } else {
     # $env:TWINE_PASSWORD = $Keys.TestPypiToken
     # twine upload --repository testpypi (Get-ChildItem -Path package) || $(throw "package upload failed")
