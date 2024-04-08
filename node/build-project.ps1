d717d2a82159bf84da9c0e1bad56b6ef85a712f9
romansertsov <roman.sertsov@postindustria.com>
2024-04-08 18:34:10 +0300

    build-project.ps1 uses local linter script command to lint folder files

--- node/build-project.ps1
+++ node/build-project.ps1
@@ -17,7 +17,7 @@ try {
         npm install
 
         Write-Output "Linting $package"
-        eslint . --ext .js
+        npm run lint
 
         if($NeedExtensions -Contains $package){
             & "$PSScriptRoot/build-extension.ps1" -PackageName $package
