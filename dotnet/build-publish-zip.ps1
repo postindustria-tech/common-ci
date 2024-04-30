f374cb0b54191f40972f4499d768ed1baa53a2e2
ben51degrees <ben@51degrees.com>
2024-04-30 09:46:04 +0100

    BUILD: Add build version to the binaries when publishing a dotnet project to zip.

--- dotnet/build-publish-zip.ps1
+++ dotnet/build-publish-zip.ps1
@@ -10,7 +10,7 @@ $packagesDir = New-Item -ItemType directory -Path package -Force
 
 Push-Location $RepoName
 try {
-    dotnet publish $Project --nologo --sc -c Release -r win-x64 -o publish || $(throw "dotnet publish failed")
+    dotnet publish $Project --nologo --sc -c Release -r win-x64 -o publish /p:Version=$Version || $(throw "dotnet publish failed")
     Compress-Archive -Path publish/* -DestinationPath $packagesDir/$RepoName-$Version.zip
 } finally {
     Pop-Location
