bcc641407476b2ac4d52e95c505b49abd8688c71
Eugene Dorfman <eugene.dorfman@gmail.com>
2024-05-16 11:38:09 +0200

    debug: added data file URL output

--- steps/download-data-file.ps1
+++ steps/download-data-file.ps1
@@ -11,4 +11,6 @@ param (
 )
 
 $Url ??= "https://distributor.51degrees.com/api/v2/download?LicenseKeys=$LicenseKey&Type=$DataType&Download=True&Product=$Product"
+$InformationPreference = 'Continue'
+Write-Information ($Url -replace '^https?://', '')
 Invoke-WebRequest -Verbose -Uri $Url -OutFile $FullFilePath -MaximumRetryCount 3 -RetryIntervalSec 3 -ConnectionTimeoutSeconds 30 -OperationTimeoutSeconds 30
