8ad7b28197fa73e93373d8c827206f9a77ad02e0
Alexander Popel <alexander.popel@postindustria.com>
2024-05-20 14:12:51 +0300

    node: use a loop in install-packages
    NPM in Node 22 seems to have changed the way it parses command line
    arguments. NPM consideres everything we pass after `install` to be a
    single argument. Using a loop instead should work around the issue.

--- node/install-package.ps1
+++ node/install-package.ps1
@@ -5,7 +5,9 @@ param (
 
 Push-Location $RepoName
 
-npm install (Get-ChildItem -Path ../package -Filter *.tgz) || $(throw "npm install failed")
+Get-ChildItem -Path ../package -Filter *.tgz | ForEach-Object {
+    npm install $_ || $(throw "npm install failed")
+}
 
 Pop-Location
 
