f92f9dfd1fe83f973521c049103fb067ea05b2e3
Jamesr51d <122470673+Jamesr51d@users.noreply.github.com>
2024-05-30 09:14:23 +0100

    Update publish-docker.ps1 - Add DryRun safeguard

--- docker/publish-docker.ps1
+++ docker/publish-docker.ps1
@@ -9,12 +9,19 @@ param(
 try {
     $Tag = "$($Keys.DockerRegistry)/$($Keys.DockerContainer):$Version"
 
+    # Login to Dockerhub
     Write-Output "Logging in to docker hub"
     docker login -u $Keys.DockerUser -p $Keys.DockerPassword
     if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-    Write-Output "Pushing docker image $Tag"
-    docker push $Tag
+    # Push to repository when dryrun is false
+    $Command = {docker push $Tag}
+    if ($DryRun -eq $False) {
+        Write-Output "Pushing docker image $Tag"
+        & $Command
+    } else {
+        Write-Output "Dry run - not executing the following: $Command"
+    }
 }
 finally {
     Write-Output "Logging out of docker hub"
