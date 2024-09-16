678d57c166ce668dfced9bfd0e2d9cf52670b895
Alexander Popel <alexander.popel@postindustria.com>
2024-09-16 12:32:51 +0300

    dotnet: add gitversion config

--- dotnet/get-next-package-version.ps1
+++ dotnet/get-next-package-version.ps1
@@ -5,7 +5,7 @@ param (
     [string]$VariableName = "Version"
 )
 
-./steps/get-next-package-version.ps1 -RepoName $RepoName -VariableName $VariableName
+./steps/get-next-package-version.ps1 -RepoName $RepoName -VariableName $VariableName -GitVersionConfigPath dotnet/gitversion.yml
 
 $SemVer = (Get-Variable -Name $VariableName).Value.SemVer
 Set-Variable -Name $VariableName -Value $SemVer -Scope Global
