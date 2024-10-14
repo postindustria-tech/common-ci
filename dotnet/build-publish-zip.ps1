823174b6733f57981b5a6150499f8543198126c5
Jamesr51d <122470673+Jamesr51d@users.noreply.github.com>
2024-10-14 16:32:24 +0100

    FEAT: add multiple project build support to build-publish-zip.ps1

--- dotnet/build-publish-zip.ps1
+++ dotnet/build-publish-zip.ps1
@@ -4,15 +4,17 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$Version,
     [string]$Project = ".",
-    [string]$Platform = "win-x64"
+    [string]$Platform = "win-x64",
+    [string]$ZipName = $RepoName
 )
 
 $packagesDir = New-Item -ItemType directory -Path package -Force
+$PublishDir = "publish-$ZipName"
 
 Push-Location $RepoName
 try {
-    dotnet publish $Project --nologo --sc -c Release -r $Platform -o publish /p:Version=$Version || $(throw "dotnet publish failed")
-    Compress-Archive -Path publish/* -DestinationPath $packagesDir/$RepoName-$Version.zip
+    dotnet publish $Project --nologo --sc -c Release -r $Platform -o $PublishDir /p:Version=$Version || $(throw "dotnet publish failed")
+    Compress-Archive -Path $PublishDir/* -DestinationPath $packagesDir/$ZipName-$Version.zip
 } finally {
     Pop-Location
 }
