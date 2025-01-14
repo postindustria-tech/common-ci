edfa72e34c7bba5cb63d8cdc3c4876c3f143f13a
Alexander Popel <alexander.popel@postindustria.com>
2025-01-14 16:12:20 +0200

    go: add performance tests runner

---
+++ go/run-performance-tests.ps1
@@ -0,0 +1,43 @@
+param (
+    [Parameter(Mandatory)][string]$RepoName,
+    [Parameter(Mandatory)][string]$OrgName,
+    [Parameter(Mandatory)][string]$Name,
+    [Parameter(Mandatory)][string]$Example,
+    [string]$ExamplesRepo,
+    [string]$Branch = "main"
+)
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
+$summaryDir = New-Item -ItemType directory -Path $RepoName/test-results/performance-summary -Force
+
+Push-Location $RepoName
+try {
+    if ($ExamplesRepo) {
+        Write-Host "Cloning examples..."
+        ./steps/clone-repo.ps1 -OrgName $OrgName -RepoName $ExamplesRepo -Branch $Branch
+    }
+
+    Write-Host "Running performance test..."
+    go run $Example
+
+    switch -File performance_report.log -Regex {
+        'Average ([^ ]+) ms per' { $MsPerDetection = [double]$matches.1 }
+        'Average ([^ ]+) detections per second' { $DetectionsPerSecond = [double]$matches.1 }
+    }
+
+    if (-not $MsPerDetection -or -not $DetectionsPerSecond) {
+        Get-Content performance_report.log | Write-Error
+    }
+
+    @{
+        HigherIsBetter = @{
+            DetectionsPerSecond = $DetectionsPerSecond
+        }
+        LowerIsBetter = @{
+            AvgMillisecsPerDetection = $MsPerDetection
+        }
+    } | ConvertTo-Json | Out-File $summaryDir/results_$Name.json
+} finally {
+    Pop-Location
+}
