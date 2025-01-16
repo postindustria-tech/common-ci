bd5e27d08b0c0baa35100b39bee7bcbb5beb2f3b
Alexander Popel <alexander.popel@postindustria.com>
2025-01-16 10:52:04 +0200

    get-next-package-version: skip if HEAD is tagged
    Avoid bumping the version if current commit is already tagged.

--- steps/get-next-package-version.ps1
+++ steps/get-next-package-version.ps1
@@ -6,7 +6,13 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
 if (-not $Version) {
-    $Version = git -C $RepoName describe --tags --abbrev=0
+    $Version = git name-rev --name-only --tags HEAD
+    if ($Version -eq 'undefined') {
+        $Version = git -C $RepoName describe --tags --abbrev=0
+    } else {
+        Write-Host "HEAD is already tagged: $Version"
+        return $Version # return existing tag if tagged
+    }
 }
 
 # Bump patch version component
