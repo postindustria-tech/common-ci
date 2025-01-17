6229f96e300098675088655f5e5c47147fccc41a
Alexander Popel <alexander.popel@postindustria.com>
2025-01-17 12:04:51 +0200

    get-next-package-version: use biggest tag
    If closest commit has multiple tags - use the biggest one. This should
    not normally happen, but if it does - this will handle it.

--- steps/get-next-package-version.ps1
+++ steps/get-next-package-version.ps1
@@ -6,9 +6,10 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
 if (-not $Version) {
-    $Version = git name-rev --name-only --tags HEAD
+    $Version = git -C $RepoName name-rev --name-only --tags HEAD
     if ($Version -eq 'undefined') {
         $Version = git -C $RepoName describe --tags --abbrev=0
+        $Version = (git -C $RepoName tag --points-at $Version "--sort=-v:refname")?[0] # ensure we're using the biggest of possible multiple tags
     } else {
         Write-Host "HEAD is already tagged: $Version"
         return $Version # return existing tag if tagged
