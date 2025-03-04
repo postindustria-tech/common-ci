1f3bb9ba95770c0d5bb9867f301ed937852fbd0d
Alexander Popel <alexander.popel@postindustria.com>
2025-03-04 09:05:29 +0200

    java: accept Name parameter
    Some scripts pass it unconditionally so we have to accept it.

--- java/build-project.ps1
+++ java/build-project.ps1
@@ -1,13 +1,15 @@
 param(
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
-    [string]$ProjectDir = "."
+    [string]$ProjectDir = ".",
+    [string]$Name
 )
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
 Push-Location $RepoName
 try {
+    Write-Host "Building $Name..."
     mvn -B install -f pom.xml -DXmx2048m -DskipTests --no-transfer-progress '-Dhttps.protocols=TLSv1.2' -DfailIfNoTests=false
 } finally {
     Pop-Location
