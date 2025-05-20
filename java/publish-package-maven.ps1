ef990cd7f5ca4d838c50283649df8efaaf8387b3
Alexander Popel <alexander.popel@postindustria.com>
2025-05-20 11:02:03 +0300

    java: less verbose publish output

--- java/publish-package-maven.ps1
+++ java/publish-package-maven.ps1
@@ -14,7 +14,7 @@ try {
     # We need to set the version here again even though the packages are already built using the next version
     # as this script will run in a new job and the repo will be cloned again.
     Write-Host "Setting version to '$Version'"
-    mvn -B versions:set "-DnewVersion=$Version"
+    mvn --batch-mode --no-transfer-progress versions:set "-DnewVersion=$Version"
 
     $env:MVN_CENTRAL_USERNAME, $env:MVN_CENTRAL_PASSWORD = $MavenSettings -split ' ', 2
     $env:MAVEN_OPTS='--add-opens=java.base/java.util=ALL-UNNAMED'
