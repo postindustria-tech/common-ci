30b647014d36c9a862f1b9dfca26020c9f520bc7
Jamesr51d <122470673+Jamesr51d@users.noreply.github.com>
2025-05-22 10:06:02 +0100

    Update start-mssql-container.ps1
    add backticks to clean up the command. 
    wrap the password in speech marks.
    remove trailing comma after the password

--- steps/start-mssql-container.ps1
+++ steps/start-mssql-container.ps1
@@ -1,8 +1,14 @@
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$Password,
+    [Parameter(Mandatory = $true)]
+    [string]$Password
 )
 
-docker run -d --name sqlcontainer --hostname sqlcontainer -p 1433:1433 -e ACCEPT_EULA=Y -e MSSQL_SA_PASSWORD=$Password -e MSSQL_PID=Developer -e MSSQL_AGENT_ENABLED=true mcr.microsoft.com/mssql/server:2022-latest
+docker run -d --name sqlcontainer --hostname sqlcontainer `
+    -p 1433:1433 `
+    -e "ACCEPT_EULA=Y" `
+    -e "MSSQL_SA_PASSWORD=$Password" `
+    -e "MSSQL_PID=Developer" `
+    -e "MSSQL_AGENT_ENABLED=true" `
+    mcr.microsoft.com/mssql/server:2022-latest
 
 Write-Output docker ps --filter "ancestor=mcr.microsoft.com/mssql/server:2022-latest"
