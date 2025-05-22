02b6116d811a16f60e0fc2fa9f7b86a294ff974c
Jamesr51d <122470673+Jamesr51d@users.noreply.github.com>
2025-05-22 09:55:03 +0100

    Update start-mssql-container.ps1

--- steps/start-mssql-container.ps1
+++ steps/start-mssql-container.ps1
@@ -1,1 +1,8 @@
-docker run -d --name sqlcontainer --hostname sqlcontainer -p 1433:1433 -e ACCEPT_EULA=Y -e MSSQL_SA_PASSWORD=admin -e MSSQL_PID=Developer -e MSSQL_AGENT_ENABLED=true mcr.microsoft.com/mssql/server:2022-latest
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$Password,
+)
+
+docker run -d --name sqlcontainer --hostname sqlcontainer -p 1433:1433 -e ACCEPT_EULA=Y -e MSSQL_SA_PASSWORD=$Password -e MSSQL_PID=Developer -e MSSQL_AGENT_ENABLED=true mcr.microsoft.com/mssql/server:2022-latest
+
+Write-Output docker ps --filter "ancestor=mcr.microsoft.com/mssql/server:2022-latest"
