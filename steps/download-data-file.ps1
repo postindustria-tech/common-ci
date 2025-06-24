bf7ecbbc2423ce3167f1082da694222679f27fe2
Alexander Popel <alexander.popel@postindustria.com>
2025-06-24 13:43:55 +0300

    download-data-file: remove debug logs

--- steps/download-data-file.ps1
+++ steps/download-data-file.ps1
@@ -12,5 +12,4 @@ if (!$Url -and (!$LicenseKey -or !$DataType -or !$Product)) {
 }
 
 $Url = $Url ? $Url : "https://distributor.51degrees.com/api/v2/download?LicenseKeys=$LicenseKey&Type=$DataType&Download=True&Product=$Product"
-Write-Host ($Url -replace '^https?://', '')
 Invoke-WebRequest -Verbose -Uri $Url -OutFile $FullFilePath -MaximumRetryCount 3 -RetryIntervalSec 3 -ConnectionTimeoutSeconds 30 -OperationTimeoutSeconds 30
